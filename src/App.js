d71ee0de215ffe9cbc79b6bd867e60b31edcde03
Rufino Rojas <lucras015@hotmail.com>
2019-02-08 11:03:05 -0600

    menu hamburger react

--- src/App.js
+++ src/App.js
@@ -1,28 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import {render} from 'react-dom';
+import CheeseburgerMenu from 'cheeseburger-menu';
+import HamburgerMenu from 'react-hamburger-menu';
+import MenuContent from './menuContent.js';
 import './App.css';
 
+const contentStyle = {
+  fontFamily: 'sans-serif',
+  textAlign: 'center',
+
+}
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+  constructor(props){
+    super(props)
+
+    this.state = {
+      menuOpen: false,
+    }
+  }
+
+  openMenu() {
+    this.setState({ menuOpen: true})
+  }
+
+  closeMenu() {
+    this.setState({ menuOpen: false})
+  }
+
+  render(){
+    return <div>
+      <CheeseburgerMenu
+        isOpen={this.state.menuOpen}
+        closeCallback ={this.closeMenu.bind(this)}>
+        <MenuContent closeCallback = {this.closeMenu.bind(this)}/>
+      </CheeseburgerMenu>
+
+      <HamburgerMenu
+        isOpen = {this.state.menuOpen}
+        menuClicked = {this.openMenu.bind(this)}
+        width={32}
+        height={24}
+        strokeWidth={3}
+        rotate={0}
+        color='black'
+        borderRadius={0}
+        animationDuration={0.5}
+      />
+
+      <div style={contentStyle}>
+        <h1>CheeseburgerMenu</h1>
+        <p>click en el icono del menu</p>
       </div>
-    );
+    </div>
   }
 }
+
 
 export default App;
